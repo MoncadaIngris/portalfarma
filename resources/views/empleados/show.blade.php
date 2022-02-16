27f4fa088ed1ce3e9ea474219bf58183fb72d969
IngrisMoncada <ingris.carcamo@unah.hn>
2022-02-16 00:39:21 -0600

    vistas para mostrar imagenes

--- resources/views/empleados/show.blade.php
+++ resources/views/empleados/show.blade.php
@@ -1,6 +1,6 @@
 @extends('plantilla.madre')
 @section('titulo')
-    Datos del Empleado {{$empleado->id}}
+    Datos del Empleado {{$empleado->nombres}} {{$empleado->apellidos}}
 @stop
 @section('contenido')
     @if(session('mensaje'))
@@ -18,11 +18,11 @@
                     <hr style="border-top: 2px solid white; border-bottom: 2px solid white; border-left:none; border-right:none; height: 2px; ">
 
                     <div  style="margin-top: 10px; width: 300px ; height: 300px; float: left; display: inline-block; margin-top: 10px; margin-bottom: 1px; margin-left: 10px; margin-right: 10px">
-                        <img src="/images/estudiante.jpg" class="card-img-top" alt="" height="90%" >
+                        <img src="{{asset($empleado->fotografia)}}" class="card-img-top" alt="" height="90%" >
                     </div>
 
                     <div style="float: left; display: inline-block; margin-top: 10px">
-                        <p style="color: white; margin-left: 20px">Nombre: <strong style="color: white">{{$empleado->nombres}} {{$empleado->apellido}}</strong></p>
+                        <p style="color: white; margin-left: 20px">Nombre: <strong style="color: white">{{$empleado->nombres}} {{$empleado->apellidos}}</strong></p>
                         <p style="color: white; margin-left: 20px">Correo Electronico: <strong style="color: white">{{$empleado->correo_electronico}}</strong></p>
                         <p style="color: white; margin-left: 20px">Telefono Personal: <strong style="color: white">{{$empleado->telefono_personal}} </strong> </p>
                         <p style="color: white; margin-left: 20px">Telefono Alternativo: <strong style="color: white">{{$empleado->telefono_alternativo}} </strong> </p>
@@ -34,8 +34,8 @@
 
                 </div>
                 <hr>
-                <div class="ln_solid" style="margin: 0 auto;">
-                    <button class="btn btn-primary" type="button" onclick="window.location='{{route('empleados.index')}}'">Regresar</button>
+                <div  style="margin: 0 auto;">
+                    <button class="btn btn-primary" type="button" onclick="window.location='javascript: history.go(-1)'">Regresar</button>
                 </div>
             </div>
         </div>
