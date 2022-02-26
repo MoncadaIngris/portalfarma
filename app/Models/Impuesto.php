22b110f0a0347ed0f9ab730e2df55e6032f32900
IngrisMoncada <ingris.carcamo@unah.hn>
2022-02-25 22:20:54 -0600

    modelos necesarios para que funcione la compra

--- app/Models/Impuesto.php
+++ app/Models/Impuesto.php
@@ -5,13 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Producto extends Model
+class Impuesto extends Model
 {
     use HasFactory;
-
-    public function concentraciones(){
-        return $this->belongsTo(Concentracion::class, 'concentracion', 'id');
-    }
 
     public function productos_temporal(){
         return $this->hasMany(Producto_Temporal::class);
---
+++ app/Models/Producto_Temporal.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Producto_Temporal extends Model
+{
+    use HasFactory;
+
+    public function productos(){
+        return $this->belongsTo(Producto::class, 'id_producto', 'id');
+    }
+
+    public function impuestos(){
+        return $this->belongsTo(Impuesto::class, 'id_impuesto', 'id');
+    }
+}
