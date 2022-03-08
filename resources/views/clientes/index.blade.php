10e53c38c6cfd8d48d0af133b07ef1aab5061517
MoncadaIngris <93739317+MoncadaIngris@users.noreply.github.com>
2022-03-08 00:41:28 -0600

    listado de empleado

---
+++ resources/views/clientes/index.blade.php
@@ -0,0 +1,51 @@
+@extends('plantilla.madre')
+@section('titulo')
+    Listado de clientes
+@stop
+@section('contenido')
+    @if(session('mensaje'))
+        <div class="alert alert-success">
+            {{session('mensaje')}}
+        </div>
+    @endif
+    <style>
+        #prueba {
+            overflow:auto;
+        }
+      </style>
+    <button class="btn btn-nuevo" onclick="window.location='{{route('clientes.create')}}'"><i class="fa-solid fa-folder-plus"></i> Agregar Cliente</button>
+
+    <table  id="datatable" class="table table-striped">
+        <thead>
+        <tr>
+            <th scope="col" style="text-align: center">Identidad</th>
+            <th scope="col" style="text-align: center">Nombres</th>
+            <th scope="col" style="text-align: center">Apellidos</th>
+            <th scope="col" style="text-align: center">Teléfono</th>
+            <th scope="col" style="text-align: center">Editar</th>
+            <th scope="col" style="text-align: center">Detalles</th>
+        </tr>
+        </thead>
+
+        <tbody>
+        @foreach ($clientes as $cliente)
+            <tr>
+                <td>{{$cliente->DNI}}</td>
+                <td>{{$cliente->nombres}}</td>
+                <td>{{$cliente->apellidos}}</td>
+                <td>{{$cliente->telefono}}</td>
+                <td>
+                    <center>
+            <a class="btn btn-editar" href="#"><i class="fa-solid fa-pen-to-square"></i></a>
+                    </center>
+                </td>
+                <td>
+                    <center>
+            <a class="btn btn-detalles" href="#"><i class="fa-solid fa-circle-info"></i></a>
+                    </center>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+@stop
