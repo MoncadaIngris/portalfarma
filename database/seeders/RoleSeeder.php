61eada28835e2d2f23df4513729dcfac216dcf06
MoncadaIngris <93739317+MoncadaIngris@users.noreply.github.com>
2022-04-04 01:19:11 -0600

    agregar rol

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -18,6 +18,7 @@ class RoleSeeder extends Seeder
 
         $roles = [
             'Admin',
+            'Desarrollador',
             'Facultativo',
             'Abastecimiento',
             'Vendedor',
