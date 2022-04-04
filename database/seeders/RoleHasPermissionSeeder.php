74927b0b5e6606af5be00293bc5551f4fc8fae30
MoncadaIngris <93739317+MoncadaIngris@users.noreply.github.com>
2022-04-04 01:19:19 -0600

    asignacion de permisos

--- database/seeders/RoleHasPermissionSeeder.php
+++ database/seeders/RoleHasPermissionSeeder.php
@@ -19,15 +19,18 @@ class RoleHasPermissionSeeder extends Seeder
         $facultativo_permissions = Permission::whereBetween('id', [1, 4]);
         $abastecimiento_permissions = Permission::whereBetween('id', [2, 11]);
         $vendedor_permissions = Permission::whereBetween('id', [10, 18]);
-        $admin_permissions = Permission::all();
+        $admin_permissions = Permission::whereBetween('id', [1, 37]);
+        $desarrollador_permissions = Permission::all();
 
             // admin
             Role::findOrFail(1)->permissions()->sync($admin_permissions->pluck('id'));
+            //desarrollador
+            Role::findOrFail(2)->permissions()->sync($desarrollador_permissions->pluck('id'));
             // Facultativo
-            Role::findOrFail(2)->permissions()->sync($facultativo_permissions->pluck('id'));
+            Role::findOrFail(3)->permissions()->sync($facultativo_permissions->pluck('id'));
             // Abastecimiento
-            Role::findOrFail(3)->permissions()->sync($abastecimiento_permissions->pluck('id'));
+            Role::findOrFail(4)->permissions()->sync($abastecimiento_permissions->pluck('id'));
             // Vendedor
-            Role::findOrFail(4)->permissions()->sync($vendedor_permissions->pluck('id'));
+            Role::findOrFail(5)->permissions()->sync($vendedor_permissions->pluck('id'));
     }
 }
