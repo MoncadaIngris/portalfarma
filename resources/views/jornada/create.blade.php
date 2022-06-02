f10e82f8aef612570f6c4e926d5e233792b4515d
Ingris Moncada <93739317+MoncadaIngris@users.noreply.github.com>
2022-06-01 19:39:01 -0600

    correccion jornada

--- resources/views/jornada/create.blade.php
+++ resources/views/jornada/create.blade.php
@@ -22,8 +22,12 @@ Añadir jornada
             <label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Nombres: <span class="required">*</span>
             </label>
             <div class="col-md-6 col-sm-6 ">
-                <input maxlength="50" type="text" id="nombres" name="nombres" required="required" class="form-control "
-                value="{{old('nombres')}}" placeholder="Ingrese el nombre de la jornada">
+                <select id="nombres" name="nombres" required="required" class="form-control ">
+                    <option value="" style="display: none">Seleccione una jornada</option>
+                    <option value="Diurno">Diurno</option>
+                    <option value="Nocturna">Nocturna</option>
+                    <option value="Mixto">Mixto</option>
+                </select>
             </div>
         </div>
 
@@ -59,38 +63,42 @@ Añadir jornada
                 var entrada = document.getElementById('entrada').value;
                 var salida = document.getElementById('salida').value;
 
-                var arrayentrada = entrada.split(':');
-                var arraysalida = salida.split(':');
+                if (entrada != '' && salida != '') {
+                    var arrayentrada = entrada.split(':');
+                    var arraysalida = salida.split(':');
 
-                var totalentrada = new Date(2011, 0, 1,arrayentrada[0],arrayentrada[1],0,0);
-                var totalsalida = new Date(2011, 0, 1, arraysalida[0],arraysalida[1],0,0);
+                    var totalentrada = new Date(2011, 0, 1,arrayentrada[0],arrayentrada[1],0,0);
+                    var totalsalida = new Date(2011, 0, 1, arraysalida[0],arraysalida[1],0,0);
 
-                var hora_entrada = totalentrada.getHours();
-                var minuto_entrada = totalentrada.getMinutes();
-                var hora_salida = totalsalida.getHours();
-                var minuto_salida = totalsalida.getMinutes();
+                    var hora_entrada = totalentrada.getHours();
+                    var minuto_entrada = totalentrada.getMinutes();
+                    var hora_salida = totalsalida.getHours();
+                    var minuto_salida = totalsalida.getMinutes();
 
-                var horas = 0;
-                if (hora_salida>=hora_entrada) {
-                    var horas = hora_salida-hora_entrada-1;
-                } else {
-                    var horas = 24-hora_entrada + hora_salida-1;
-                }
-
-                var minutos = 60 - minuto_entrada + minuto_salida; 
-                
-                if (minutos == 60) {
-                    minutos = 0;
-                    horas ++;
-                }else{
-                    if(minutos > 60){
-                        minutos = minutos - 60;
-                        horas ++;
+                    var horas = 0;
+                    if (hora_salida>=hora_entrada) {
+                        var horas = hora_salida-hora_entrada-1;
+                    } else {
+                        var horas = 24-hora_entrada + hora_salida-1;
                     }
+
+                    var minutos = 60 - minuto_entrada + minuto_salida; 
+                    
+                    if (minutos == 60) {
+                        minutos = 0;
+                        horas ++;
+                    }else{
+                        if(minutos > 60){
+                            minutos = minutos - 60;
+                            horas ++;
+                        }
+                    }
+                    
+                    document.getElementById('total').value =horas+' horas y '+minutos+' minutos';
+                    
+                } else {
+                    document.getElementById('total').value = 'Calculo de horas laborales';
                 }
-                
-                document.getElementById('total').value =horas+' horas y '+minutos+' minutos';
-                
                 
             }
         </script>
