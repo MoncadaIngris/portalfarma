5043301d54736a83f992b8b0e6675ad9942e90f7
Juanfer201 <juanillooe@gmail.com>
2022-06-07 11:46:36 -0600

    relaciones modelos

--- app/Models/Laboral.php
+++ app/Models/Laboral.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Laboral extends Model
 {
     use HasFactory;
+
+    public function empleado()
+    {
+        return $this->belongsTo(Empleado::class,'id_empleado', 'id');
+    }
+
+    public function entrada()
+    {
+        return $this->belongsTo(Hora_entrada::class,'id_he', 'id');
+    }
+
+    public function salida()
+    {
+        return $this->belongsTo(Hora_salida::class,'id_hs', 'id');
+    }
+
 }
