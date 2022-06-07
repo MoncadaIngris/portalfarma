f4fa21d842e725b8f453a6f87ee5178fc7cd7864
Juanfer201 <juanillooe@gmail.com>
2022-06-07 11:45:43 -0600

    entrada y salida base de datos

---
+++ database/migrations/2022_06_06_212705_create_hora_entradas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHoraEntradasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hora_entradas', function (Blueprint $table) {
+            $table->id();
+            $table->time("hora");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hora_entradas');
+    }
+}
