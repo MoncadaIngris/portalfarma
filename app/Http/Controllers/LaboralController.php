ccc2ed63a4325a9f5b6668c461794f74c1ee98ec
Juanfer201 <juanillooe@gmail.com>
2022-06-07 11:46:56 -0600

    funciones del listado laboral

--- app/Http/Controllers/LaboralController.php
+++ app/Http/Controllers/LaboralController.php
@@ -3,16 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Models\Laboral;
+use App\Models\Empleado;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class LaboralController extends Controller
 {
     public function index()
     {
-        abort_if(Gate::denies('laborales_index'),redirect()->route('welcome')->with('denegar','No tiene acceso a esta sección'));
+        //abort_if(Gate::denies('laborales_index'),redirect()->route('welcome')->with('denegar','No tiene acceso a esta sección'));
 
-        $laborales= Laboral::where('estado',0)->select("id","nombres", "hora_entrada", "hora_salida","horas_ordinarias", "horas_extras")->get();
+        $laborales= Laboral::orderby("fecha", "desc")->get();
 
-        return view('laborales/index')->with('laborales', $laborales);
+        return view('laboral/index')->with('laborales', $laborales);
+    }
+
+    public function carga()
+    {
+        //abort_if(Gate::denies('laborales_index'),redirect()->route('welcome')->with('denegar','No tiene acceso a esta sección'));
+        $fecha_actual = date("Y-m-d");
+        $fecha_moderna = date("d-m-Y");
+        $actual= Laboral::where('fecha',$fecha_actual)->get();
+
+        if(count($actual) >0){
+            $laborales= Laboral::where('fecha',$fecha_actual)->get();
+            return view('laboral/index')
+            ->with('laborales', $laborales)
+            ->with('alerta', "Los datos de los empleados del ".$fecha_moderna." fueron cargados anteriormente");
+        }else{
+            $empleados = Empleado::where('estado',0)->get();
+
+            foreach ($empleados as $emp) {
+                $laboral = new Laboral();
+
+                $laboral->id_empleado = $emp->id;
+                $laboral->fecha = $fecha_actual;
+
+                $creado = $laboral->save();
+            }
+            if ($creado) {
+                $laborales= Laboral::where('fecha',$fecha_actual)->get();
+                return view('laboral/index')
+                ->with('laborales', $laborales)
+                ->with('confirmacion', "Los datos de los empleados del ".$fecha_moderna." fueron cargados exitosamente");
+            }
+        }
     }
 }
