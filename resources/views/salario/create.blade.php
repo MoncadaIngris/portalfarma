973b17632293755e41177f82b0b2d1fa5df08c65
MoncadaIngris <ingris.carcamo@unah.hn>
2022-06-13 22:33:44 -0600

    formulario de creacion

---
+++ resources/views/salario/create.blade.php
@@ -0,0 +1,114 @@
+@extends('plantilla.madre')
+@section('titulo')
+Añadir Salario hora
+@stop
+@section('contenido')
+<div class="x_content">
+    <br />
+    @if($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>
+                        {{$error}}
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="item form-group">
+            <label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Seleccione la jornada: <span class="required">*</span>
+            </label>
+            <div class="col-md-6 col-sm-6 ">
+                <select name="jornada" id="jornada" class="form-control" required onchange="horas()">
+                        <option value="" style="display: none">Seleccione una jornada</option>
+                        @foreach ($jornada as $jorn)
+                            <option value="{{$jorn->id}}">{{$jorn->nombre}}</option>
+                        @endforeach
+                    </select>
+                </select>
+            </div>
+        </div>
+
+        <div class="item form-group">
+            <label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Horas laborales semanales: <span class="required">*</span>
+            </label>
+            <div class="col-md-6 col-sm-6 ">
+                <input type="text" class="form-control" value="0" readonly name="hsem" id="hsem">
+            </div>
+        </div>
+
+        <div class="item form-group">
+            <label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Horas laborales diarias: <span class="required">*</span>
+            </label>
+            <div class="col-md-6 col-sm-6 ">
+                <input type="text" class="form-control" value="0" readonly name="hdia" id="hdia">
+            </div>
+        </div>
+
+        <div class="item form-group">
+            <label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Salario semanal: <span class="required">*</span>
+            </label>
+            <div class="col-md-6 col-sm-6 ">
+                <input type="number" class="form-control" name="semanal" id="semanal" placeholder="Ingrese el salario semanal"
+                min="0" max="999999.99" maxlength="10" type="number" step="any" required title="Formato de precio incorrecto">
+            </div>
+        </div>
+
+        <div class="item form-group">
+            <label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Salario diario: <span class="required">*</span>
+            </label>
+            <div class="col-md-6 col-sm-6 ">
+                <input type="text" class="form-control" name="diario" id="diario" value="0.00" readonly>
+            </div>
+        </div>
+
+        <div class="item form-group">
+            <label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Salario por hora: <span class="required">*</span>
+            </label>
+            <div class="col-md-6 col-sm-6 ">
+                <input type="text" class="form-control" name="hora" id="hora" value="0.00" readonly>
+            </div>
+        </div>
+
+        <div class="ln_solid"></div>
+        <div class="item form-group">
+            <div class="col-md-6 col-sm-6 offset-md-3">
+                <button class="btn btn-regresar" type="button" onclick="window.location='{{route('salariohora.index')}}'">Cancelar</button>
+                <a type="button" href="javascript:location.reload()" class="btn btn-limpiar">Limpiar</a>
+                <button type="submit" class="btn btn-guardar">Guardar</button>
+            </div>
+        </div>
+
+    </form>
+
+    <script>
+        function horas(){
+            var jornada = document.getElementById('jornada').value;
+            @foreach ($jornada as $jorn)
+                if (jornada == {{$jorn->id}}) {
+                    var hora = {{$jorn->diferencia}};
+                    if (hora< 0) {
+                        hora = 24+hora;
+                    }
+
+                    document.getElementById('hsem').value = hora*6;
+                    document.getElementById('hdia').value = hora;
+                }
+            @endforeach
+        }
+        setInterval('salario()',1000);
+        function salario(){
+            var semanal = document.getElementById('semanal').value;
+            var hora = document.getElementById('hdia').value;
+            if (semanal != "") {
+                document.getElementById('diario').value = (Math.round((semanal/6)*100))/100;
+                document.getElementById('hora').value = (Math.round(((semanal/6)/hora)*100))/100;
+            }
+        }
+    </script>
+</div>
+@stop
