a6b1ecd4da1955cbc6fadcada33c30bd1c287181
Juanfer201 <juanillooe@gmail.com>
2022-06-25 17:46:07 -0600

    ordenar vista

---
+++ resources/views/baucher/index.blade.php
@@ -0,0 +1,46 @@
+@extends('plantilla.madre')
+@section('titulo')
+    Listado de baucher
+@stop
+@section('contenido')
+    @if(session('mensaje'))
+        <div class="alert alert-success">
+
+            {{session('mensaje')}}
+        </div>
+    @endif
+    <style>
+        #prueba {
+            overflow:auto;
+        }
+      </style>
+    
+    <table  id="datatable" class="table table-striped">
+        <thead>
+        <tr>
+            <th scope="col" class="sorting" style="text-align: center">Fecha de inicio</th>
+            <th scope="col" class="sorting" style="text-align: center">Fecha final</th>
+            <th scope="col" class="sorting" style="text-align: center">Total ingresos</th>
+            <th scope="col" class="sorting" style="text-align: center">Total deducciones</th>
+            <th scope="col" class="sorting" style="text-align: center">Total a cobrar</th>
+            <th scope="col" class="sorting" style="text-align: center">Detalles</th>        
+        </tr>
+        </thead>
+            @foreach ($baucher as $b)
+                <tr>
+                    <td>{{$b->planillas->fecha_inicio}}</td>
+                    <td>{{$b->planillas->fecha_final}}</td>
+                    <td style="text-align: right">L. {{number_format(($b->precio_hora*$b->hora_ordinaria)+($b->precio_hora*$b->hora_extra),2)}}</td>
+                    <td style="text-align: right">L. {{number_format(($b->deducciones),2)}}</td>
+                    <td style="text-align: right">L. {{number_format(($b->precio_hora*$b->hora_ordinaria)+($b->precio_hora*$b->hora_extra)-($b->deducciones),2)}}</td>
+                    <td>
+                        <center>
+                            <a class="btn btn-detalles" href="{{route("baucher.show",["id"=>$b->id_planilla])}}"><i class="fa-solid fa-circle-info"></i></a>
+                        </center>
+                    </td>
+                </tr>
+            @endforeach
+        <tbody>
+        </tbody>
+    </table>
+@stop
