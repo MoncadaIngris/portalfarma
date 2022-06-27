5ec33869e0f2a83c5ce69af13eb512df42ea7649
ruthfonseca <89227992+ruthfonseca@users.noreply.github.com>
2022-06-28 01:22:15 +0200

    Update show.blade.php

--- resources/views/planilla/show.blade.php
+++ resources/views/planilla/show.blade.php
@@ -105,15 +105,15 @@
     <table  class="table table-striped table-bordered">
         <thead>
         <tr>
-             <th scope="col" class="sorting" style="text-align: center;" rowspan="2">Empleado</th>
+              <th scope="col" class="sorting" style="text-align: center;" rowspan="2">Empleado</th>
             <th scope="col" class="sorting" style="text-align: center;" rowspan="2">Cargo</th>
             <th scope="col" class="sorting" style="text-align: center;" rowspan="2">Precio por hora</th>
-            <th scope="col" class="sorting" style="text-align: center;" colspan="2">Lunes</th>
-            <th scope="col" class="sorting" style="text-align: center;" colspan="2">Martes</th>
-            <th scope="col" class="sorting" style="text-align: center;" colspan="2">Miércoles</th>
-            <th scope="col" class="sorting" style="text-align: center;" colspan="2">Jueves</th>
-            <th scope="col" class="sorting" style="text-align: center;" colspan="2">Viernes</th>
-            <th scope="col" class="sorting" style="text-align: center;" colspan="2">Sábado</th>
+            <th scope="col" class="sorting" style="text-align: center;" colspan="3">Lunes</th>
+            <th scope="col" class="sorting" style="text-align: center;" colspan="3">Martes</th>
+            <th scope="col" class="sorting" style="text-align: center;" colspan="3">Miércoles</th>
+            <th scope="col" class="sorting" style="text-align: center;" colspan="3">Jueves</th>
+            <th scope="col" class="sorting" style="text-align: center;" colspan="3">Viernes</th>
+            <th scope="col" class="sorting" style="text-align: center;" colspan="3">Sábado</th>
             <th scope="col" class="sorting" style="text-align: center;" rowspan="2">Total horas</th>
             <th scope="col" class="sorting" style="text-align: center;" rowspan="2">Sub-Total</th>
             <th scope="col" class="sorting" style="text-align: center;" rowspan="2">Deducción</th>
