1d166aeadeb16d45116313def4c1096422d1dd8e
MoncadaIngris <93739317+MoncadaIngris@users.noreply.github.com>
2022-08-03 10:08:59 -0600

    Update create.blade.php
    change

--- resources/views/vacaciones/create.blade.php
+++ resources/views/vacaciones/create.blade.php
@@ -34,14 +34,14 @@ Asignar vacaciones
             </div>
         </div>
         <div class="item form-group">
-            <label class="col-form-label col-md-3 col-sm-3 label-align" for="last-name">Correo electronico: <span class="required">*</span>
+            <label class="col-form-label col-md-3 col-sm-3 label-align" for="last-name">Correo electrónico: <span class="required">*</span>
             </label>
             <div class="col-md-6 col-sm-6 ">
             <input  type="text" class="form-control" value="{{$empleado->correo_electronico}}" disabled>
             </div>
         </div>
         <div class="item form-group">
-            <label class="col-form-label col-md-3 col-sm-3 label-align" for="last-name">Telefono: <span class="required">*</span>
+            <label class="col-form-label col-md-3 col-sm-3 label-align" for="last-name">Télefono: <span class="required">*</span>
             </label>
             <div class="col-md-6 col-sm-6 ">
             <input  type="text" class="form-control" value="{{$empleado->telefono_personal}}" disabled>
