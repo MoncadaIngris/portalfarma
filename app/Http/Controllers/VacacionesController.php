2ef9832ae51288948a533e75be7f3720de584fcc
MoncadaIngris <ingris.carcamo@unah.hn>
2022-08-08 14:05:52 -0600

    Update VacacionesController.php

--- app/Http/Controllers/VacacionesController.php
+++ app/Http/Controllers/VacacionesController.php
@@ -138,12 +138,12 @@ class VacacionesController extends Controller
         $mensaje=[
             'inicio.required' => 'La fecha de inicio no puede estar vacía',
             'inicio.date' => 'La fecha de inicio debe de ser una fecha valida',
-            'inicio.before' => 'La fecha de inicio debe de ser anterior a '.$maxima2,
+            'inicio.before' => 'La fecha de inicio debe de ser anterior a la fecha final',
             'inicio.after' => 'La fecha de inicio debe de ser posterior a '.$minima,
             'final.required' => 'La fecha de final no puede estar vacía',
             'final.date' => 'La fecha de final debe de ser una fecha valida',
             'final.before' => 'La fecha de final debe de ser anterior a '.$maxima,
-            'final.after' => 'La fecha de final debe de ser posterior a '.$minima2,
+            'final.after' => 'La fecha de final debe de ser posterior a la fecha de inicio',
         ];
 
         $this->validate($request,$rules,$mensaje);
