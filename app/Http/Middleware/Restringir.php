0fca456e42bfea1cc668f99342c26ace338eee26
MoncadaIngris <ingris.carcamo@unah.hn>
2022-08-08 08:30:06 -0600

    Update Restringir.php

--- app/Http/Middleware/Restringir.php
+++ app/Http/Middleware/Restringir.php
@@ -10,12 +10,14 @@ class Restringir
 {
     public function handle(Request $request, Closure $next)
     {
-        if (auth()->user()->empleados->vacaciones->inicio <= date("Y-m-d") && auth()->user()->empleados->vacaciones->final >= date("Y-m-d")) {
-Auth::logout();
-        
-            // redireccionamos a donde queremos
-            return redirect()->route('login')
-            ->with('a', 'activar');
+        if (isset(auth()->user()->empleados->vacaciones->inicio)) {
+            if (auth()->user()->empleados->vacaciones->inicio <= date("Y-m-d") && auth()->user()->empleados->vacaciones->final >= date("Y-m-d")) {
+
+                Auth::logout();
+                
+                return redirect()->route('login')
+                ->with('a', 'activar');
+            }
         }
         return $next($request);
 
